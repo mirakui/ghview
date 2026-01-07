f3caeef8d1ea6939b66d2bd2497debd6344d9293
Issei Naruta <20384+mirakui@users.noreply.github.com>
2026-01-07 12:19:50 +0900

    create a helloworld app

---
+++ src/App.test.tsx
@@ -0,0 +1,10 @@
+import { render, screen } from "@testing-library/react";
+import { describe, it, expect } from "vitest";
+import App from "./App";
+
+describe("App", () => {
+  it("displays Hello, World", () => {
+    render(<App />);
+    expect(screen.getByText("Hello, World")).toBeInTheDocument();
+  });
+});
